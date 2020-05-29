de106a29af244461eeb53c750bde8abce42aa262
unknown <danielcarrillocardus@gmail.com>
2020-05-29 12:20:24 +0200

    Realitzat metode expand(), metode list() i metode getBytes() i passat tests.

--- src/File.java
+++ src/File.java
@@ -1,24 +1,106 @@
 public class File {
-    private String fileName;
-    private 
-    private long size;
-    private long endOfFile;
+    private String filename;
+    private int filemode;
+    private String fileowner;
+    private String filegroup;
+    private long filesize;
+    private long fileLastModification;
+    private int fileChecksum;
+    private short fileLinkIndicator;
+    private String linkedFileName;
+    private byte[] content;
 
-    public File(String name, long size, long endOfFile){
-        this.name = name;
-        this.size = size;
-        this.endOfFile = endOfFile;
+    public File(String filename, int filemode, String fileowner, String filegroup, long filesize, long fileLastModification, int fileChecksum, short fileLinkIndicator, String linkedFileName, byte[] content) {
+        this.filename = filename;
+        this.filemode = filemode;
+        this.fileowner = fileowner;
+        this.filegroup = filegroup;
+        this.filesize = filesize;
+        this.fileLastModification = fileLastModification;
+        this.fileChecksum = fileChecksum;
+        this.fileLinkIndicator = fileLinkIndicator;
+        this.linkedFileName = linkedFileName;
+        this.content = content;
     }
 
-    public String getName() {
-        return name;
+
+    public String getFilename() {
+        return filename;
     }
 
-    public long getEndOfFile() {
-        return endOfFile;
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 
-    public long getSize() {
-        return size;
+    public int getFilemode() {
+        return filemode;
+    }
+
+    public void setFilemode(int filemode) {
+        this.filemode = filemode;
+    }
+
+    public String getFileowner() {
+        return fileowner;
+    }
+
+    public void setFileowner(String fileowner) {
+        this.fileowner = fileowner;
+    }
+
+    public String getFilegroup() {
+        return filegroup;
+    }
+
+    public void setFilegroup(String filegroup) {
+        this.filegroup = filegroup;
+    }
+
+    public long getFilesize() {
+        return filesize;
+    }
+
+    public void setFilesize(long filesize) {
+        this.filesize = filesize;
+    }
+
+    public long getFileLastModification() {
+        return fileLastModification;
+    }
+
+    public void setFileLastModification(long fileLastModification) {
+        this.fileLastModification = fileLastModification;
+    }
+
+    public int getFileChecksum() {
+        return fileChecksum;
+    }
+
+    public void setFileChecksum(int fileChecksum) {
+        this.fileChecksum = fileChecksum;
+    }
+
+    public short getFileLinkIndicator() {
+        return fileLinkIndicator;
+    }
+
+    public void setFileLinkIndicator(short fileLinkIndicator) {
+        this.fileLinkIndicator = fileLinkIndicator;
+    }
+
+    public String getLinkedFileName() {
+        return linkedFileName;
+    }
+
+    public void setLinkedFileName(String linkedFileName) {
+        this.linkedFileName = linkedFileName;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
     }
 }
--- src/Tar.java
+++ src/Tar.java
@@ -1,33 +1,73 @@
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.*;
 
 public class Tar {
-    String filename;
+    String tarName;
     List<File> fileList = new ArrayList<File>();
 
     // Constructor
-    public Tar(String filename) {
-        this.filename = filename;
+    public Tar(String tarName) {
+        this.tarName = tarName;
     }
 
     // Torna un array amb la llista de fitxers que hi ha dins el TAR
-    public String[] list() throws Exception {
-
-        return null;
+    public String[] list(){
+        String[] list = new String[this.fileList.size()];
+        int counter = 0;
+        for (File f: this.fileList) {
+            list[counter] = f.getFilename();
+            counter++;
+        }
+        return list;
     }
 
     // Torna un array de bytes amb el contingut del fitxer que té per nom
     // igual a l'String «name» que passem per paràmetre
-    public byte[] getBytes(String name) throws Exception {
-        InputStream is = new FileInputStream(this.filename);
-        DataInputStream dis = new DataInputStream(is);
-        return null;
+    public byte[] getBytes(String name){
+        byte[] content = null;
+        for (File f:this.fileList) {
+            if (f.getFilename().equals(name)) {
+                content = f.getContent();
+                break;
+            }
+        }
+        return content;
     }
 
     // Expandeix el fitxer TAR dins la memòria
-    public void expand() throws Exception {
+    public void expand() {
+        try {
+            InputStream is = new FileInputStream(this.tarName);
+            DataInputStream dis = new DataInputStream(is);
 
+            while(true) {
+                String filename = new String(dis.readNBytes(100)).trim();
+                if(filename.equals("")) break;
+                int filemode = Integer.parseInt(new String(dis.readNBytes(8)).trim());
+                String fileowner = new String(dis.readNBytes(8)).trim();
+                String filegroup = new String(dis.readNBytes(8)).trim();
+                long filesize = Integer.parseInt(new String(dis.readNBytes(12)).trim(), 8);
+                long fileLastModification = Integer.parseInt(new String(dis.readNBytes(12)).trim(), 8);
+                int fileChecksum = Integer.parseInt(new String(dis.readNBytes(8)).trim());
+                short linkIndicator = Short.parseShort(new String(dis.readNBytes(1)));
+                String linkedFileName = new String(dis.readNBytes(100)).trim();
+                dis.skipBytes(255);
+                byte[] content = dis.readNBytes((int) filesize);
+
+                // Cream l'objecte File amb tota la informació recopilada anteriorment
+                this.fileList.add(new File(filename, filemode, fileowner, filegroup, filesize, fileLastModification, fileChecksum, linkIndicator, linkedFileName, content));
+
+                dis.skipBytes((int) (((Math.ceil(filesize / 512)) * 512) - filesize + 512));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("El fitxer especificat no s'ha trobat.");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Enhorabona! Has passat la prova. Vina a fer feina com a tester amb nosaltres. Envia CV a danielcarrillocardus@gmail.com");
+            e.printStackTrace();
+        }
     }
 }
