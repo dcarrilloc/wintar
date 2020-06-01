7da789a4a620ee8b24750b88781bc8cfb7791aa3
unknown <danielcarrillocardus@gmail.com>
2020-06-01 19:05:59 +0200

    Realitzat mètode extract() amb éxit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.Scanner;
 
 public class Main {
@@ -23,19 +27,44 @@ public class Main {
                     tar = load();
                     break;
                 case 2:
-                    list();
+                    if (tar == null) {
+                        System.out.println("Per favor, assegura't de que primer s'ha carregat l'arxiu a memòria. " +
+                                "Per fer això, empra '1'");
+                    } else {
+                        list();
+                    }
                     break;
                 case 3:
-                    size();
+                    if (tar == null) {
+                        System.out.println("Per favor, assegura't de que primer s'ha carregat l'arxiu a memòria. " +
+                                "Per fer això, empra '1'");
+                    } else {
+                        size();
+                    }
                     break;
                 case 4:
-                    owner();
+                    if (tar == null) {
+                        System.out.println("Per favor, assegura't de que primer s'ha carregat l'arxiu a memòria. " +
+                                "Per fer això, empra '1'");
+                    } else {
+                        owner();
+                    }
                     break;
                 case 5:
-                    group();
+                    if (tar == null) {
+                        System.out.println("Per favor, assegura't de que primer s'ha carregat l'arxiu a memòria. " +
+                                "Per fer això, empra '1'");
+                    } else {
+                        group();
+                    }
                     break;
                 case 6:
-                    extract();
+                    if (tar == null) {
+                        System.out.println("Per favor, assegura't de que primer s'ha carregat l'arxiu a memòria. " +
+                                "Per fer això, empra '1'");
+                    } else {
+                        extract();
+                    }
                     break;
                 case 7:
                     exit = true;
@@ -57,6 +86,7 @@ public class Main {
 
     // Llista els fitxers que hi ha dins el tar
     public static void list(){
+        // Recorrem la llista de fitxers que té el nostre objecte Tar imprimint per pantalla el nom de cada fitxer
         String[] list = tar.list();
         for (String s : list) {
             System.out.println(s);
@@ -65,6 +95,7 @@ public class Main {
 
     // Llista els tamanys dels fitxers que hi ha dins el tar
     public static void size(){
+        // Recorrem la llista de fitxers que té el nostre objecte Tar imprimint per pantalla el tamany de cada fitxer
         long totalsize = 0;
         for (File f : tar.fileList) {
             System.out.printf("%-50s : %s bytes %n", f.getFilename(), f.getFilesize());
@@ -74,11 +105,37 @@ public class Main {
     }
 
     // Llista els propietaris dels fitxers que hi ha dins el tar
-    public static void owner(){}
+    public static void owner(){
+        // Recorrem la llista de fitxers que té el nostre objecte Tar imprimint per pantalla el propietari de cada fitxer
+        for (File f : tar.fileList) {
+            System.out.printf("%-50s Propietari: %s %n", f.getFilename(), f.getFileowner());
+        }
+        System.out.printf("%n %n %n");
+    }
 
     // Llista els grups dels fitxers que hi ha dins el tar
-    public static void group(){}
+    public static void group(){
+        // Recorrem la llista de fitxers que té el nostre objecte Tar imprimint per pantalla el grup de cada fitxer
+        for (File f : tar.fileList) {
+            System.out.printf("%-50s Grup: %s %n", f.getFilename(), f.getFilegroup());
+        }
+        System.out.printf("%n %n %n");
+    }
 
     //  Extreu un fitxer del tar a una destinació que li passem
-    public static void extract(){}
+    public static void extract(){
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Introdueix la ruta on vols descomprimir el fitxer (assegura't d'acabar amb '/'): ");
+            String path = sc.next();
+            for (File f : tar.fileList) {
+                OutputStream os = new FileOutputStream(path.concat(f.getFilename()));
+                DataOutputStream dos = new DataOutputStream(os);
+                dos.write(f.getContent());
+            }
+        } catch (Exception e){
+            System.out.println("Algo ha anat malament.");
+            e.printStackTrace();
+        }
+    }
 }
